97b70d20c714a78a7bbfafd2a8eccad70f6e6c5d
Mincong Zhang <mincong.zhang@hotmail.com>
2015-03-18 22:33:48 +0000

    create

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player1;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+		//player = GameObject.Find("Player").GetComponent<PlayerController>().speed; 
+		//get the "ball" Player without using mouse.
+
+	}
+
+	// Update is called once per frame
+	//LateUpdate gathering last known states, run after Update()
+	void LateUpdate () {
+		transform.position = player1.transform.position + offset;
+	}
+}
+
+//http://unity3d.com/learn/tutorials/projects/roll-a-ball/pickup-objects
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	void FixedUpdate(){
+		float move_horizontal = Input.GetAxis ("Horizontal");
+		float move_vertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (move_horizontal, 0.0f, move_vertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+		//http://unity3d.com/learn/tutorials/projects/roll-a-ball/moving-the-player
+	}
+}
